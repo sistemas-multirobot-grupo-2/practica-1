7f850fb82693ad5c7b0f5fb89144da6fe89edbdd
José Miguel <36297047+jmtc7@users.noreply.github.com>
2019-03-04 01:46:00 +0100

    Develope "read" functions and "Data" class

--- scripts/aurigapy/sensors.py
+++ scripts/aurigapy/sensors.py
@@ -21,7 +21,9 @@ IMPOSSIBLE_DISTANCE = -1.0
 class Data:
     def __init__(self):
         print("Init Class Data")
-        self.ultrasensor_distance = 0
+        self.ultrasensor_distance = -1 #Distancia en cm leida por el sensor de distancia. "-1" para UNKNOWN
+        self.read_light = -1 #Valor entre 0 y 1024 de intensidad de luz. "-1" para UNKNOWN
+        self.line_detection = -1 #0-los 2 on; 1-izq on; 2-der on; 3-ninguno. "-1" para UNKNOWN
 
 # Este struct contendrá la información procesada de los sensores
 class Information:
@@ -44,13 +46,50 @@ def readUltraSensor(robot,port):
 
 # Función específica para leer el sensor de luz
 def readLightSensor(robot,port):
-    if(robot.mode == 'simulation'): 
-        print(robot.name + ": Leemos sensor de luz en el puerto " + str(port)) 
+"""
+Funcion para leer el sensor de luz
+
+:param self: Referencia a la instancia desde la que se llama a la funcion
+:param port: Puerto donde esta conectado el sensor de luz.
+
+:return: True si no ha habido ningun problema. False en cualquier otro caso (ERROR)
+"""
+    error = False #Variable que contiene el valor de retorno
+    
+    if robot.mode == 'simulation':
+        print(robot.name + ": Procesamos la información del sensor de luz en el puerto " + str(port))
+    else:
+        robot.st_data.read_ligth = robot.get_light_sensor_onboard(port) #Leer datos del sensor
+
+        #Comprobar si la lectura da un valor logico
+        if robot.st_data.read_ligth<1 or robot.st_data.read_ligth>1023:
+            error = True
+    
+    return error
+
 
 # Función específica para leer el sensor de linea
 def readLineSensor(robot,port):
-    if(robot.mode == 'simulation'): 
-        print(robot.name + ": Leemos sensor de linea en el puerto " + str(port)) 
+"""
+Funcion para leer el sensor de linea
+
+:param robot: Referencia a la instancia desde la que se llama a la funcion
+:param port: Puerto donde esta conectado el sensor de linea.
+
+:return: True si no ha habido ningun problema. False en cualquier otro caso (ERROR)
+"""
+    error = False #Variable que contiene el valor de retorno
+    
+    if robot.mode == 'simulation':
+        print(robot.name + ": Procesamos la información del sensor de linea en el puerto " + str(port))
+    else: 
+        robot.st_data.line_detection = robot.get_line_sensor(port) #Leer informacion -> 0-los 2 on; 1-izq on; 2-der on; 3-ninguno
+        
+        #Si se recibe un valor no-valido
+        if robot.st_data.line_detection<0 or robot.st_data.line_detection>3:
+            error = True #Indicar fallo
+        
+        return error #Devolver la variable que indica si ha habido algun fallo
                 
 
 ##------------------Procesado------------------##  
@@ -58,16 +97,42 @@ def readLineSensor(robot,port):
 # Procesador específico para extaer información acerca de la presencia de obstáculos en base a los datos del 
 # sensor de ultrasonidos.
 def processUltrasonicSensorData(robot,port):
-    if(robot.mode == 'simulation'):
+"""
+Funcion para extaer informacion acerca de la presencia de obstaculos en base a los datos del sensor de distancia (ultrasonidos)
+
+:param self: Referencia a la instancia desde la que se llama a la funcion
+:param port: Puerto donde esta conectado el sensor de distancia (ultrasonidos).
+
+:return: True si no ha habido ningun problema. False en cualquier otro caso (ERROR)
+"""
+    if robot.mode == 'simulation':
         print(robot.name + ": Procesamos la información del sensor de ultrasonidos en el puerto " + str(port))
         
+    
 # Procesador específico para extaer información acerca de la presencia de una fuente lumínica en base a los datos del 
 # sensor de luz.
 def processLightSensorData(robot,port):
-    if(robot.mode == 'simulation'):
+"""
+Funcion para extaer informacion acerca de la presencia de obstaculos en base a los datos del sensor de luz
+
+:param self: Referencia a la instancia desde la que se llama a la funcion
+:param port: Puerto donde esta conectado el sensor de luz.
+
+:return: True si no ha habido ningun problema. False en cualquier otro caso (ERROR)
+"""
+    if robot.mode == 'simulation':
         print(robot.name + ": Procesamos la información del sensor de luz en el puerto " + str(port))
 
+        
 # Procesador específico para extaer información acerca de la presencia de una linea.
 def processLineSensorData(robot,port):
-    if(robot.mode == 'simulation'):
+"""
+Funcion para extaer informacion acerca de la presencia de obstaculos en base a los datos del sensor de linea
+
+:param robot: Referencia a la instancia desde la que se llama a la funcion
+:param port: Puerto donde esta conectado el sensor de linea.
+
+:return: True si no ha habido ningun problema. False en cualquier otro caso (ERROR)
+"""
+    if robot.mode == 'simulation':
         print(robot.name + ": Procesamos la información del sensor de linea en el puerto " + str(port))
